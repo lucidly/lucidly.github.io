5fac5785139d9a0683592805c1c7a08eb793d963
Lucid <lucidly@outlook.com>
2017-04-04 18:27:57 -0700

    some style changes

---
+++ assets/script.js
@@ -0,0 +1,65 @@
+// some really hacked together responsive navigation
+function responsiveNav() {
+	
+	if ($(window).width() <= 700) {
+		// show hamburger navigation
+		$('.navToggle').show();
+		$('nav').addClass('responsiveNav');
+		$('nav').addClass('closed');
+	
+		$('.navToggle').click(function() {	
+			if ($('nav').hasClass('open')) {
+				$('nav').removeClass('open');
+				$('nav').addClass('closed');
+			} else if ($('nav').hasClass('closed')) {
+				$('nav').removeClass('closed');
+				$('nav').addClass('open');
+			}
+		});
+		
+	} else {
+		$('nav').removeClass('responsiveNav');
+		$('nav').removeClass('open');
+		$('nav').removeClass('closed');
+		$('.navToggle').hide();
+	}
+	
+}
+
+// responsive navigation on resize event
+$(window).resize(responsiveNav);
+
+
+// fullPage.js functionality
+$(document).ready(function() {
+
+	$('#fullpage').fullpage({
+		verticalCentered: true,
+		anchors: ['home', 'about', 'contact'],
+		sectionsColor: ['#fdb7b7', '#313131', '#fff'],
+		slidesNavigation: true,
+		menu: 'nav',
+		paddingTop: $('nav').height(),
+		paddingBottom: $('nav').height(),
+		scrollOverflow: true,
+		recordHistory: false,
+		
+		onLeave: function(index, nextIndex, direction) {
+
+			// change navigation text to black on white pages
+			if ((index != 3  && nextIndex == 3)) {
+				$('nav').addClass('black');
+			}
+		
+			// change back to white on non-white pages
+			if ((index == 3) && (nextIndex == 1 || nextIndex == 2)) {
+				$('nav').removeClass('black');
+			}
+		}
+		
+	});
+	
+	responsiveNav();
+
+
+});
